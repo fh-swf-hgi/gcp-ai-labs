44cdf1b8acb8404f75cdaa4b68014ce47c0afcc3
Heiner Giefers <hgiefers@users.noreply.github.com>
2025-06-06 12:09:10 +0200

    Update build_and_deploy.sh

--- ImageTag_CloudRun/build_and_deploy.sh
+++ ImageTag_CloudRun/build_and_deploy.sh
@@ -7,7 +7,7 @@ echo "Using username: ${username}"
 if [ ! -z "$username" ]
 then
     tar cfz source.tgz Dockerfile main.py  requirements.txt
-    gcloud builds submit source.tgz  --region=europe-west1 --project cloud-computing-ss24 --tag=gcr.io/cloud-computing-ss24/${username}-imagetag
+    gcloud builds submit source.tgz  --region=europe-west1 --project cloud-computing-ss25 --tag=gcr.io/cloud-computing-ss25/${username}-imagetag
     rm source.tgz
-    gcloud run deploy ${username}-imagetag-service --project cloud-computing-ss24 --image gcr.io/cloud-computing-ss24/${username}-imagetag --allow-unauthenticated
+    gcloud run deploy ${username}-imagetag-service --project cloud-computing-ss25 --image gcr.io/cloud-computing-ss25/${username}-imagetag --allow-unauthenticated
 fi
